bc6a1bb1740d4ebcdd83a18f218cafc81ea592b0
Ganesh Pawar <128301890+ganeshppawar12@users.noreply.github.com>
2025-01-31 20:26:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { useAuth } from "./AuthProvider";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import Dashboard from "./components/Dashboard";
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
 
-      <Route path="/login" element={<LoginPage />}></Route>
-      <Route path='/' element={ <Dashboard />}></Route>
+      <Route path="/" element={<LoginPage />}></Route>
+      <Route path='/dashboard' element={ <Dashboard />}></Route>
 
 
     </Routes>
           
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
